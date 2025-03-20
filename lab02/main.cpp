80e9ccf82a0804a188acc966ec420a829fdcaa3c
renan <renanneves430@gmail.com>
2025-03-20 14:55:06 -0300

    updates

--- lab02/main.cpp
+++ lab02/main.cpp
@@ -4,49 +4,112 @@
 #include <glm/glm.hpp>
 #include <iostream>
 #include <cstring>
+#include <glm/gtc/matrix_transform.hpp>
+#include <string>
+#include <fstream>
+#include <sstream>
 
-std::vector<glm::vec3> vertices;
-std::vector<glm::ivec3> faces;
+struct Vertex {
+    glm::vec3 position;
+};
 
-bool loadOBJ(const std::string &path) {
-    FILE* file = fopen(path.c_str(), "r");
-    if (file == NULL) {
-        std::cerr << "Failed to open file: " << path << std::endl;
+std::vector<Vertex> vertices;
+std::vector<unsigned int> indices;
+
+bool loadOBJ(const std::string& path) {
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open OBJ file: " << path << std::endl;
         return false;
     }
 
-    while (true) {
-        char lineHeader[128];
-        int res = fscanf(file, "%s", lineHeader);
-        
-        if (res == EOF) {
-            break;
-        }
+    std::vector<glm::vec3> temp_positions;
+    std::vector<unsigned int> temp_indices;
 
-        if (strcmp(lineHeader, "v") == 0) {
-            glm::vec3 auxVertices;
-            fscanf(file, "%f %f %f\n", &auxVertices.x, &auxVertices.y, &auxVertices.z);
-            vertices.push_back(auxVertices);
-        } 
-        else if (strcmp(lineHeader, "f") == 0) {
-            glm::ivec3 auxFaces;
-            fscanf(file, "%d %d %d\n", &auxFaces.x, &auxFaces.y, &auxFaces.z);
-            faces.push_back(auxFaces);
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream s(line);
+        std::string type;
+        s >> type;
+
+        if (type == "v") {
+            glm::vec3 position;
+            s >> position.x >> position.y >> position.z;
+            temp_positions.push_back(position);
+        } else if (type == "f") {
+            unsigned int vIndex[3];
+            for (int i = 0; i < 3; i++) {
+                s >> vIndex[i];
+                temp_indices.push_back(vIndex[i] - 1);
+            }
         }
     }
 
-    fclose(file);
+    for (size_t i = 0; i < temp_indices.size(); i++) {
+        vertices.push_back({temp_positions[temp_indices[i]]});
+    }
+    
+    indices = temp_indices;
     return true;
 }
 
-int render(std::vector<glm::vec3> vertices, std::vector<glm::ivec3> faces) {
+GLuint createShaderProgram() {
+    const char* vertexShader = R"(
+        #version 330 core
+        layout(location = 0) in vec3 aPos;
+        
+        uniform mat4 model;
+    
+        void main() {
+            gl_Position = model * vec4(aPos, 1.0);
+        })";
+    
+
+    const char* fragmentShader = R"(
+        #version 330 core
+        out vec4 FragColor;
+        void main() {
+            FragColor = vec4(1.0, 1.0, 1.0, 1.0); // White color
+        })";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, fs);
+    glAttachShader(shaderProgram, vs);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    return shaderProgram;
+}
+
+glm::vec3 getCentroModelo() {
+    glm::vec3 min = vertices[0].position;
+    glm::vec3 max = vertices[0].position;
+
+    for (const auto& vertex : vertices) {
+        min = glm::min(min, vertex.position);
+        max = glm::max(max, vertex.position);
+    }
+
+    return (min + max); 
+}
+
+
+int render() {
     GLFWwindow* window;
 
     if (!glfwInit()) {
         return -1;
     }
 
-    window = glfwCreateWindow(640, 480,"Tela", nullptr, nullptr);
+    window = glfwCreateWindow(640, 480,"OBJ Viewer", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -60,10 +123,57 @@ int render(std::vector<glm::vec3> vertices, std::vector<glm::ivec3> faces) {
         return -1;
     }
 
+    GLuint shaderProgram = createShaderProgram();
+
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    
+        glUseProgram(shaderProgram);
+    
+        // matriz de escala
+        glm::vec3 modelCenter = getCentroModelo();
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, -modelCenter); 
+        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
+        
+        GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
+        
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+        glBindVertexArray(0);
+    
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
-
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -75,7 +185,5 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    render(vertices, faces);
-
-    return 0;
+    return render();
 }
