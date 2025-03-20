e7238bf372606c9f42e8d7548a620ea071f98577
renan <renanneves430@gmail.com>
2025-03-20 14:23:02 -0300

    coleta de dados pronta (talvez), fazer o render

--- lab02/main.cpp
+++ lab02/main.cpp
@@ -5,16 +5,10 @@
 #include <iostream>
 #include <cstring>
 
+std::vector<glm::vec3> vertices;
+std::vector<glm::ivec3> faces;
 
-struct Vertex {
-    glm::vec3 position;
-    glm::vec3 normal;
-};
-
-std::vector<Vertex> vertices;
-std::vector<unsigned int> indices;
-
-bool loadOBJ(const std::string& path) {
+bool loadOBJ(const std::string &path) {
     FILE* file = fopen(path.c_str(), "r");
     if (file == NULL) {
         std::cerr << "Failed to open file: " << path << std::endl;
@@ -24,23 +18,64 @@ bool loadOBJ(const std::string& path) {
     while (true) {
         char lineHeader[128];
         int res = fscanf(file, "%s", lineHeader);
+        
         if (res == EOF) {
             break;
         }
 
         if (strcmp(lineHeader, "v") == 0) {
-            glm::vec3 vertex;
-            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
-            vertices.push_back({vertex, glm::vec3(0.0f)});
+            glm::vec3 auxVertices;
+            fscanf(file, "%f %f %f\n", &auxVertices.x, &auxVertices.y, &auxVertices.z);
+            vertices.push_back(auxVertices);
         } 
         else if (strcmp(lineHeader, "f") == 0) {
-            unsigned int vertexIndex[3];
-            unsigned int normalIndex[3];
-            fscanf(file, "%d//%d//%d//%d//%d//%d\n", &vertexIndex[0], &normalIndex[0], &vertexIndex[1], &normalIndex[1], &vertexIndex[2], &normalIndex[2]);
-
-            indices.push_back(vertexIndex[0] - 1);
-            indices.push_back(vertexIndex[1] - 1);
-            indices.push_back(vertexIndex[2] - 1);
+            glm::ivec3 auxFaces;
+            fscanf(file, "%d %d %d\n", &auxFaces.x, &auxFaces.y, &auxFaces.z);
+            faces.push_back(auxFaces);
         }
     }
+
+    fclose(file);
+    return true;
+}
+
+int render(std::vector<glm::vec3> vertices, std::vector<glm::ivec3> faces) {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480,"Tela", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "ERROR: GLEW Initialization Failed\n";
+        return -1;
+    }
+
+
+    return 0;
+}
+
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <obj file>" << std::endl;
+        return 1;
+    }
+
+    if (!loadOBJ(argv[1])) {
+        return 1;
+    }
+
+    render(vertices, faces);
+
+    return 0;
 }
