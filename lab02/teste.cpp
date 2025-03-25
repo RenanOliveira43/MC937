bccd73c701a2751075d7beacc0f91b848f1076ae
renan <renanneves430@gmail.com>
2025-03-25 12:07:02 -0300

    modelo gira

--- lab02/teste.cpp
+++ lab02/teste.cpp
@@ -2,10 +2,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <iostream>
-#include <cstring>
 #include <glm/gtc/matrix_transform.hpp>
-
+#include <iostream>
+#include <cstdio>
 
 struct Vertex {
     glm::vec3 position;
@@ -17,42 +16,38 @@ std::vector<unsigned int> indices;
 bool loadOBJ(const std::string& path) {
     FILE* file = std::fopen(path.c_str(), "r");
     if (!file) {
-        std::cerr << "Failed to open OBJ file: " << path << std::endl;
+        std::cerr << "Erro ao abrir arquivo: " << path << std::endl;
         return false;
     }
 
-    std::vector<glm::vec3> temp_positions; 
-    std::vector<unsigned int> temp_indices; 
+    std::vector<glm::vec3> temp_positions;
+    std::vector<unsigned int> temp_indices;
 
     char line[128];
     while (std::fgets(line, sizeof(line), file)) {
-        if (line[0] == 'v') { 
+        if (line[0] == 'v' && line[1] == ' ') { 
             glm::vec3 vertex;
             std::sscanf(line, "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
-            
             temp_positions.push_back(vertex);
         } 
         else if (line[0] == 'f') {
             unsigned int vIndex[3];
-            std::sscanf(line, "f %d %d %d", &vIndex[0], &vIndex[1], &vIndex[2]);
-            
+            std::sscanf(line, "f %u %u %u", &vIndex[0], &vIndex[1], &vIndex[2]);
+
             temp_indices.push_back(vIndex[0] - 1);
             temp_indices.push_back(vIndex[1] - 1);
             temp_indices.push_back(vIndex[2] - 1);
         }
     }
-    
-    vertices.clear();
-    for (unsigned int index : temp_indices) {
-        if (index < temp_positions.size()) {
-            Vertex vertex;
-            vertex.position = temp_positions[index];
-            vertices.push_back(vertex);
-        }
-    }
-    
-    std::fclose(file);
 
+    vertices.clear();
+    indices = temp_indices;
+
+    for (const auto& pos : temp_positions) {
+        vertices.push_back({ pos });
+    }
+
+    std::fclose(file);
     return true;
 }
 
@@ -60,19 +55,20 @@ GLuint createShaderProgram() {
     const char* vertexShader = R"(
         #version 330 core
         layout(location = 0) in vec3 aPos;
-        
+
         uniform mat4 model;
-    
+        uniform mat4 view;
+        uniform mat4 projection;
+
         void main() {
-            gl_Position = model * vec4(aPos, 1.0);
+            gl_Position = projection * view * model * vec4(aPos, 1.0);
         })";
-    
 
     const char* fragmentShader = R"(
         #version 330 core
         out vec4 FragColor;
         void main() {
-            FragColor = vec4(1.0, 1.0, 1.0, 1.0); // White color
+            FragColor = vec4(1.0, 1.0, 1.0, 1.0);
         })";
 
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
@@ -84,10 +80,12 @@ GLuint createShaderProgram() {
     glCompileShader(fs);
 
     GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, fs);
     glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
     glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
 
     return shaderProgram;
 }
@@ -101,43 +99,46 @@ glm::vec3 getCentroModelo() {
         max = glm::max(max, vertex.position);
     }
 
-    return (min + max) / 2.0f; 
+    return (min + max) / 2.0f;
 }
 
 int render() {
-    GLFWwindow* window;
-
     if (!glfwInit()) {
         return -1;
     }
 
-    window = glfwCreateWindow(800, 600,"Modelo", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OBJ Viewer 3D", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
-
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
-        std::cerr << "ERROR: GLEW Initialization Failed\n";
+        std::cerr << "Erro ao inicializar GLEW" << std::endl;
         return -1;
     }
 
-    GLuint shaderProgram = createShaderProgram();
+    glEnable(GL_DEPTH_TEST);
 
-    GLuint VAO, VBO;
+    GLuint shaderProgram = createShaderProgram();
+    glUseProgram(shaderProgram);
+
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    
+    glGenBuffers(1, &EBO);
 
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -145,40 +146,45 @@ int render() {
 
     glm::vec3 modelCenter = getCentroModelo();
     glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(0, 2, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+    GLuint viewLoc = glGetUniformLocation(shaderProgram, "view");
+    GLuint projLoc = glGetUniformLocation(shaderProgram, "projection");
 
     float angle = 0.0f;
 
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-    
-        glUseProgram(shaderProgram);
-    
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         angle += 0.01f;
-    
         model = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 1.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-        
-        GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        model = glm::translate(model, -modelCenter);
+        model = glm::scale(model, glm::vec3(8.0f, 8.0f, 8.0f));
+
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &model[0][0]);
-        
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, &projection[0][0]);
+
+        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
-    
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     glfwDestroyWindow(window);
     glfwTerminate();
-
     return 0;
 }
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        std::cerr << "Invalid input" << std::endl;
+        std::cerr << "Uso: " << argv[0] << " <arquivo.obj>" << std::endl;
         return 1;
     }
 
